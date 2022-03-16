daaa2ad0ede770df853a32a9c1be182ea500d39f
Ephron Martin <ephronmartin2016@gmail.com>
2022-03-16 17:27:24 +0530

    add get_data

---
+++ src/get_data.py
@@ -0,0 +1,34 @@
+##read params
+## process
+## Return dataframe
+
+from webbrowser import get
+import yaml
+import os
+import pandas as pd
+import argparse
+
+def read_yaml(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    data_path = config['data_source']['s3_source']
+    df = pd.read_csv(data_path)
+    print(df)
+
+
+
+
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parsed_args = args.parse_args()
+
+    get_data(config_path = parsed_args.config) 
